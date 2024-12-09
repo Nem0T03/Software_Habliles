4adc8a636baaa9b53f79c08a72d6e492c607db67
1_L0V3_P3T <152706890+Nem0T03@users.noreply.github.com>
2024-12-10 04:33:51 +0700

    Delete script.sh
    Signed-off-by: 1_L0V3_P3T <152706890+Nem0T03@users.noreply.github.com>

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Đường dẫn tới file chứa mật khẩu SSH
-password_file="/home/binh/password.txt"
-
-# Thực hiện rsync với mật khẩu từ file
-sudo rsync -avzhe "sshpass -f $password_file ssh"  binh@192.168.30.3:/var/www/html/mywebsite/ /var/www/html/mywebsite/
